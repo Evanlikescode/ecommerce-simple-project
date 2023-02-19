ac683335cf04e8140f5bb23ef60f06aad35b8e7e
evan <evan258@Rahayus-MacBook-Air.local>
2023-02-19 23:45:44 +0700

    change the structure

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask
+from flaskext.mysql import MySQL
 import os
-
 application = Flask(__name__)
 
 # connection
@@ -8,11 +8,17 @@ application.config['MYSQL_HOST'] = os.environ.get("MYSQL_HOST")
 application.config['MYSQL_DB'] = os.environ.get("MYSQL_DB")
 application.config['MYSQL_USER'] = os.environ.get("MYSQL_USER")
 application.config['MYSQL_PASSWORD'] = os.environ.get("MYSQL_PASSWORD")
-
+mysql = MySQL()
+mysql.init_app(application)
 
 @application.route('/')
-def test():
-    return 'test'
+def login():
+    cur = mysql.get_db().cursor()
+    cur.execute("SELECT * FROM tb_user")
+    row = cur.fetchall()
+    print(row)
+    return ''
+    
 
 if __name__ == "__main__":
     application.run(debug=True)
